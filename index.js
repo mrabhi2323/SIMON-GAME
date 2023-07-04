366238161800173d5a1e8b9c609ba2ad6fc7a896
mrabhi2323 <119652678+mrabhi2323@users.noreply.github.com>
2023-07-04 22:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,79 @@
 
-var numberOfDrumButtons= document.querySelectorAll(".drum").length;
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-for(var i=0;i<numberOfDrumButtons;i++)
-{    document.querySelectorAll("button")[i].addEventListener("click",function()
-        {
-            var buttonInnerHTML= this.innerHTML;
-            makeSound(buttonInnerHTML);
-            buttonAnimation(buttonInnerHTML);
-            
-        });
-    
-}
+var gamePattern = [];
+var userClickedPattern = [];
 
-document.addEventListener("keypress",function(event)
+var started = false;
+var level = 0;
 
-{
-    makeSound(event.key);
-    buttonAnimation(event.key);
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
 
-function makeSound(key)
-{
-    switch(key)
-            {
-                case "w":
-                
-                    var tom1=new Audio("sounds/tom-1.mp3");
-                    tom1.play();
-                    break;
-                case "a":
-                
-                    var tom2=new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-                    break;
-                case "s":
-                
-                    var tom3=new Audio("sounds/tom-3.mp3");
-                    tom3.play();
-                    break;
-                case "d":
-                
-                    var tom4=new Audio("sounds/tom-4.mp3");
-                    tom4.play();
-                    break;
-                case "j":
-                
-                    var crash=new Audio("sounds/crash.mp3");
-                    crash.play();
-                    break;
-                case "k":
-                
-                    var kickbass=new Audio("sounds/kick-bass.mp3");
-                    kickbass.play();
-                    break;
-                case "l":
-                
-                    var snare=new Audio("sounds/snare.mp3");
-                    snare.play();
-                    break;
-            }
-}
+$(".btn").click(function() {
 
-function buttonAnimation(currentkey)
-{
-    var activeButton= document.querySelector("." + currentkey);
-    activeButton.classList.add("pressed");
-    function myfunc()
-    {
-        activeButton.classList.remove("pressed");
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
     }
-    setTimeout(myfunc,100);
 }
 
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
